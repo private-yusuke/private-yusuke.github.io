9b8cb3dad899e64e774792a73da890d1f1f83ad1
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-01-12 03:36:44 +0900

    Initial commit for v2

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-143785591-1');
+
+require("@fortawesome/fontawesome-free/js/all");
