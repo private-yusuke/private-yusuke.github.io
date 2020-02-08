513b2cd38ff8902ab6ea1c539155156bc6010866
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-02-08 19:12:23 +0900

    remove everything for v3

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'UA-143785591-1');
-
-require("@fortawesome/fontawesome-free/js/all");
